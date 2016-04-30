7be3ad61b12b07f1df68f7b47ad535bea642549c
Arpit Maheshwari <arp19690@gmail.com>
2016-04-30 12:47:10 +0530

    admobs integrated

--- moojik/build/generated/source/r/release/de/psdev/licensesdialog/R.java
+++ moojik/build/generated/source/r/release/de/psdev/licensesdialog/R.java
@@ -20,8 +20,8 @@ public final class R {
 		public static final int notices = 0x7f070009;
 	}
 	public static final class string {
-		public static final int notices_close = 0x7f0801ab;
-		public static final int notices_default_style = 0x7f0801ac;
-		public static final int notices_title = 0x7f0801ad;
+		public static final int notices_close = 0x7f0801a9;
+		public static final int notices_default_style = 0x7f0801aa;
+		public static final int notices_title = 0x7f0801ab;
 	}
 }
