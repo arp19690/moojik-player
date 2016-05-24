5ec54c4e1767dd7e4f661db44aa02ba1bf5154d6
Arpit Maheshwari <arp19690@gmail.com>
2016-05-24 15:18:51 +0530

    changes

--- app/src/main/java/com/moojik/music/player/utils/Helpers.java
+++ app/src/main/java/com/moojik/music/player/utils/Helpers.java
@@ -16,11 +16,8 @@ package com.moojik.music.player.utils;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Paint;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
@@ -33,6 +30,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
 import com.moojik.music.player.R;
 
 public class Helpers {
@@ -56,11 +54,11 @@ public class Helpers {
 
     public static class AboutDialog extends DialogFragment {
 
-        String urlgooglelus = "https://plus.google.com/u/0/+NamanDwivedi14";
-        String urlcommunity = "https://plus.google.com/communities/111029425713454201429";
-        String urltwitter = "https://twitter.com/naman1405";
-        String urlgithub = "https://github.com/naman14";
-        String urlsource = "https://github.com/naman14/Timber/issues";
+//        String urlgooglelus = "https://plus.google.com/u/0/+NamanDwivedi14";
+//        String urlcommunity = "https://plus.google.com/communities/111029425713454201429";
+//        String urltwitter = "https://twitter.com/naman1405";
+//        String urlgithub = "https://github.com/naman14";
+//        String urlsource = "https://github.com/naman14/Timber/issues";
 
         public AboutDialog() {
         }
@@ -74,11 +72,11 @@ public class Helpers {
 
             TextView appversion = (TextView) aboutBodyView.findViewById(R.id.app_version_name);
 
-            TextView googleplus = (TextView) aboutBodyView.findViewById(R.id.googleplus);
-            TextView twitter = (TextView) aboutBodyView.findViewById(R.id.twitter);
-            TextView github = (TextView) aboutBodyView.findViewById(R.id.github);
-            TextView source = (TextView) aboutBodyView.findViewById(R.id.source);
-            TextView community = (TextView) aboutBodyView.findViewById(R.id.feature_request);
+//            TextView googleplus = (TextView) aboutBodyView.findViewById(R.id.googleplus);
+//            TextView twitter = (TextView) aboutBodyView.findViewById(R.id.twitter);
+//            TextView github = (TextView) aboutBodyView.findViewById(R.id.github);
+//            TextView source = (TextView) aboutBodyView.findViewById(R.id.source);
+//            TextView community = (TextView) aboutBodyView.findViewById(R.id.feature_request);
 
             TextView dismiss = (TextView) aboutBodyView.findViewById(R.id.dismiss_dialog);
             dismiss.setOnClickListener(new View.OnClickListener() {
@@ -87,58 +85,59 @@ public class Helpers {
                     dismiss();
                 }
             });
-            googleplus.setPaintFlags(googleplus.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            twitter.setPaintFlags(twitter.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-            github.setPaintFlags(github.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+//            googleplus.setPaintFlags(googleplus.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+//            twitter.setPaintFlags(twitter.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+//            github.setPaintFlags(github.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
-            googleplus.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlgooglelus));
-                    startActivity(i);
-                }
-
-            });
-            twitter.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urltwitter));
-                    startActivity(i);
-                }
-
-            });
-            github.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlgithub));
-                    startActivity(i);
-                }
-
-            });
-            source.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlsource));
-                    startActivity(i);
-                }
-            });
-            community.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(Intent.ACTION_VIEW);
-                    i.setData(Uri.parse(urlcommunity));
-                    startActivity(i);
-                }
-            });
+//            googleplus.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent i = new Intent(Intent.ACTION_VIEW);
+//                    i.setData(Uri.parse(urlgooglelus));
+//                    startActivity(i);
+//                }
+//
+//            });
+//            twitter.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent i = new Intent(Intent.ACTION_VIEW);
+//                    i.setData(Uri.parse(urltwitter));
+//                    startActivity(i);
+//                }
+//
+//            });
+//            github.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent i = new Intent(Intent.ACTION_VIEW);
+//                    i.setData(Uri.parse(urlgithub));
+//                    startActivity(i);
+//                }
+//
+//            });
+//            source.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent i = new Intent(Intent.ACTION_VIEW);
+//                    i.setData(Uri.parse(urlsource));
+//                    startActivity(i);
+//                }
+//            });
+//            community.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    Intent i = new Intent(Intent.ACTION_VIEW);
+//                    i.setData(Uri.parse(urlcommunity));
+//                    startActivity(i);
+//                }
+//            });
             try {
                 PackageInfo pInfo = getActivity().getPackageManager().getPackageInfo(getActivity().getPackageName(), 0);
                 String version = pInfo.versionName;
                 int versionCode = pInfo.versionCode;
-                appversion.setText("Moojik Player " + version);
+//                appversion.setText("Moojik Player " + version);
+                appversion.setText("Moojik Player");
             } catch (PackageManager.NameNotFoundException e) {
                 e.printStackTrace();
             }
